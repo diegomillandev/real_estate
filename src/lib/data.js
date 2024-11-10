d9639712f16664950770bbeef36e2901cd1a2329
diegomillandev <millan@iMac-Pro.local>
2024-11-09 22:17:16 -0500

    add data project

---
+++ src/lib/data.js
@@ -0,0 +1,45 @@
+import project_img_1 from "../assets/project_img_1.jpg";
+import project_img_2 from "../assets/project_img_2.jpg";
+import project_img_3 from "../assets/project_img_3.jpg";
+import project_img_4 from "../assets/project_img_4.jpg";
+import project_img_5 from "../assets/project_img_5.jpg";
+import project_img_6 from "../assets/project_img_6.jpg";
+
+export const projectsData = [
+  {
+    title: "Skyline Haven",
+    price: "$2,50,000",
+    location: "California",
+    image: project_img_1,
+  },
+  {
+    title: "Vista Verde",
+    price: "$2,50,000",
+    location: "San Francisco",
+    image: project_img_2,
+  },
+  {
+    title: "Serenity Suites",
+    price: "$2,50,000",
+    location: "Chicago",
+    image: project_img_3,
+  },
+  {
+    title: "Central Square",
+    price: "$2,50,000",
+    location: "Los Angeles",
+    image: project_img_4,
+  },
+  {
+    title: "Vista Verde",
+    price: "$2,50,000",
+    location: "San Francisco",
+    image: project_img_5,
+  },
+  {
+    title: "Serenity Suites",
+    price: "$2,50,000",
+    location: "Chicago",
+    image: project_img_6,
+  },
+];
