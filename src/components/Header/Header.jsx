c37af145767ed76695bb1860096dccbf3c5ae4c1
diegomillandev <millan@iMac-Pro.local>
2024-11-10 07:00:25 -0500

    add title page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,26 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
   return (
-    <div className="bg-hero-pattern min-h-screen bg-cover bg-center flex items-center w-full overflow-hidden">
+    <div className="bg-hero-pattern min-h-screen bg-cover bg-center flex justify-center items-center w-full overflow-hidden">
+      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
+        <h2 className="text-5xl sm:text-6xl md:text-[5.5rem] inline-block max-w-3xl font-semibold pt-20">
+          Explore homes that fit your dreams
+        </h2>
+        <div className="mt-10 inline-flex gap-x-4">
+          <a
+            href="#projects"
+            className="py-2 px-4 rounded border transition-colors duration-150 border-white hover:bg-gray-50/10"
+          >
+            Projects
+          </a>
+          <a
+            href="#contact"
+            className="py-2 px-4 rounded transition-colors duration-150 bg-blue-700 hover:bg-blue-600"
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
       <Navbar />
     </div>
   );
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -9,13 +9,17 @@ export const Navbar = () => {
         <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
           <img src={assets.logo} alt="logo" />
 
-          <ul className="hidden md:flex gap-7 text-white [&>li]:hover:cursor-pointer relative">
-            {navItems.map((item, index) => (
-              <li key={index} className="menu-item py-1 px-2">
-                {item.title}
-              </li>
-            ))}
-          </ul>
+          <nav aria-label="Main Navigation">
+            <ul className="hidden md:flex gap-7 text-white [&>li]:hover:cursor-pointer relative">
+              {navItems.map((item, index) => (
+                <li key={index} className="menu-item py-1 px-2">
+                  <a href={item.href} className="text-white">
+                    {item.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
           <button
             className="hidden md:block bg-white text-slate-700 py-2 px-6 rounded-full hover:bg-gray-200"
@@ -27,6 +31,10 @@ export const Navbar = () => {
             }
           >
             Contact
+          </button>
+
+          <button className="md:hidden w-7">
+            <img src={assets.menu_icon} alt="" />
           </button>
         </div>
       </div>
