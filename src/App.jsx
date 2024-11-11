610bbf7a85f28f13246f68251d67890dad136449
diegomillandev <millan@iMac-Pro.local>
2024-11-10 22:46:15 -0500

    add api send emails

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { ToastContainer } from "react-toastify";
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
 import { Header } from "./components/Header";
 import { Projects } from "./components/Projects";
 import { Testimonials } from "./components/Testimonials";
+import "react-toastify/dist/ReactToastify.css";
 
 export const App = () => {
   return (
@@ -12,6 +14,7 @@ export const App = () => {
       <Projects />
       <Testimonials />
       <Contact />
+      <ToastContainer />
     </div>
   );
 };
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
 import { Section, TitleSection } from "../shared";
+import { toast } from "react-toastify";
 
 export const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", import.meta.env.VITE_WEB3FORMS_API_KEY);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      toast.success("Message sent successfully");
+      setResult("");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error("Error sending message");
+      setResult("");
+    }
+  };
+
   return (
     <Section id="Contact">
       <TitleSection
@@ -8,7 +37,11 @@ export const Contact = () => {
         highlight="With Us"
         subtitle="Ready to Make a Move? Let's Build Your Future Together"
       />
-      <form action="" className="w-full md:w-[650px] mt-10 ">
+      <form
+        action=""
+        className="w-full md:w-[650px] mt-10 "
+        onSubmit={onSubmit}
+      >
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="">
             <label htmlFor="yourName" className="mb-2 block">
@@ -17,8 +50,10 @@ export const Contact = () => {
             <input
               type="text"
               id="yourName"
+              name="name"
               placeholder="Full Name"
               className="w-full p-3 border border-gray-300 rounded"
+              required
             />
           </div>
           <div className="">
@@ -28,8 +63,10 @@ export const Contact = () => {
             <input
               type="email"
               id="yourEmail"
+              name="email"
               placeholder="Email Address"
               className="w-full p-3 border border-gray-300 rounded"
+              required
             />
           </div>
           <div className="md:col-span-2">
@@ -37,17 +74,18 @@ export const Contact = () => {
               Message
             </label>
             <textarea
-              name=""
+              name="message"
               id="message"
               cols="30"
               rows="10"
               placeholder="Message"
               className=" w-full p-3 border border-gray-300 rounded resize-none"
+              required
             ></textarea>
           </div>
         </div>
         <button className="w-full bg-blue-700 hover:bg-blue-600 text-white py-3 rounded font-medium mt-6">
-          Send Message
+          {result ? result : "Send Message"}
         </button>
       </form>
     </Section>
