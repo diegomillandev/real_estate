6d5b51b7f6fd292ccdf57eb0d56f47307cc0c190
diegomillandev <millan@iMac-Pro.local>
2024-11-11 09:17:09 -0500

    add framer animation project

--- src/components/shared/Section.jsx
+++ src/components/shared/Section.jsx
@@ -1,9 +1,14 @@
 import { cn } from "@/utils/utils";
 import PropTypes from "prop-types";
+import { motion } from "framer-motion";
 
 export const Section = ({ id, children, classNames }) => {
   return (
-    <section
+    <motion.section
+      initial={{ opacity: 0, y: 100 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
       id={id}
       className={cn(
         "flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden",
@@ -11,7 +16,7 @@ export const Section = ({ id, children, classNames }) => {
       )}
     >
       {children}
-    </section>
+    </motion.section>
   );
 };
 
